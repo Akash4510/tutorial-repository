4e3c1c61f136b27e3a53adc476c4f9cab40cd2af
Akash <test.email.cr7@gmail.com>
2021-12-04 19:43:43 +0530

    Added greet function in server.py

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
 print("I am currently the most popular programming language in the world.")
 print("Modified after adding the file in for tracking.")
 print("I only want upto this line.")
+
+
+def greet(name):
+    print(f"Hello, {name}\nNice to meet you!")
